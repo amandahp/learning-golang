c4d8e422c7a1a2e9046bb4596eeff42760b0d25c
amandahp <amandahoffmann96@gmail.com>
2021-05-19 20:05:12 -0300

    feat: types and var

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,19 @@
 package main //pacote principal
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+//tipo
 
 func main() {
+	cidade := "Cascavel" //inferência de tipo outra maneira de declarar var
 	var nome string = "Douglas"
 	var idade int
 	var versao float32 = 1.1
-	fmt.Println("Olá, sr.", nome, idade)
+	fmt.Println("Olá, sr.", nome, idade, ". Mora na cidade", cidade)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variavel é", reflect.TypeOf(versao))
 }
