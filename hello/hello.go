495bd79199916c2e6deda152038c1986fefbce6e
amandahp <amandahoffmann96@gmail.com>
2021-05-19 20:00:00 -0300

    feat: finished class

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,9 @@ package main //pacote principal
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var nome string = "Douglas"
+	var idade int
+	var versao float32 = 1.1
+	fmt.Println("Olá, sr.", nome, idade)
+	fmt.Println("Este programa está na versão", versao)
 }
