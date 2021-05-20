8de6294591c39cecba6480097bf724c4f1bffaf7
amandahp <amandahoffmann96@gmail.com>
2021-05-20 00:47:00 -0300

    feat: switch and if

--- aplication/aplication.go
+++ aplication/aplication.go
@@ -17,6 +17,26 @@ func main() {
 	var comando int
 	// fmt.Scanf("%d", &comando) // primeiro modificador, endereço do comando (&)
 	fmt.Scan(&comando)
-
 	fmt.Println("O comando escolhido foi:", comando)
+
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do programa")
+	// } else {
+	// 	fmt.Println("Não conheço este comando")
+	// }
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+	default:
+		fmt.Println("Não conheço este comando")
+	}
 }
